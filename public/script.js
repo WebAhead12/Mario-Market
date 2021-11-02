da1790869c5598dfbebb2e8a95ed77c86db04a42
halakhamis07 <78309585+halakhamis07@users.noreply.github.com>
2021-11-02 11:16:18 +0200

    product button and fetch

--- public/script.js
+++ public/script.js
@@ -1,11 +1,31 @@
 const input = document.querySelector(".productInputs");
-
+const button = document.querySelector(".buttonContainer");
 input.addEventListener("keyup", (event) => {
-  fetch(`data/${input.value}`)
+  fetch(`autocomplete/${input.value}`)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     //creates the profile picture and the followings
-    .then((data) => {});
+    .then((data) => {
+      if (Object.keys(data).length == 0) {
+        elDiv.textInput = "There is no such product";
+        etabdiv.style.display = "none"; //lma etab tsme
+      }
+    });
+});
+
+button.addEventListener("click", () => {
+  const inputSearch = input.value;
+  input.value = "";
+  if (inputSearch == "") {
+    products.innerHTML = "";
+    return;
+  }
+  fetch(`data/${inputSearch.value}`)
+    .then((response) => {
+      if (!response.ok) throw new Error(response.status);
+      return response.json();
+    })
+    .then({});
 });
